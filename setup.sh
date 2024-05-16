9a2c46642a7a9740defd55340c05718346364e74
Cheng Shao <terrorjack@type.dance>
2024-05-16 22:15:29 +0000

    Enable opensuse apt repo for ubuntu-24.04

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-if [[ "$(lsb_release --release --short)" == "22.04" ]]; then
-  curl -f -L --retry 5 "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key" | sudo gpg --dearmor -o /usr/share/keyrings/devel_kubic_libcontainers_unstable.gpg
+VERSION="$(lsb_release --release --short)"
 
-  echo "deb [signed-by=/usr/share/keyrings/devel_kubic_libcontainers_unstable.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04 /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list
-fi
+curl -f -L --retry 5 "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_$VERSION/Release.key" | sudo gpg --dearmor -o /usr/share/keyrings/devel_kubic_libcontainers_unstable.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/devel_kubic_libcontainers_unstable.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_$VERSION /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list
 
 sudo apt update
 
